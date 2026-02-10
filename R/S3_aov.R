d941ebc9590e36fe6ba397bb1367babf8060ab20
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-09 20:27:07 -0500

    tzh

---
+++ R/S3_aov.R
@@ -0,0 +1,29 @@
+
+if (FALSE) {
+  ?stats::aov
+  aov(breaks ~ tension + wool, data = warpbreaks) |>
+    summary() |>
+    class() # "summary.aov" "listof"
+  # must write [.pval.summary.aov()] so that not to dispatch to [.pval.listof()] !!!
+}
+
+#' @rdname pval
+#' @export .pval.anova
+#' @export
+.pval.anova <- function(x) {
+  ret <- x[, 'Pr(>F)']
+  names(ret) <- rownames(x) # nrow-1 drops rownames
+  return(ret)
+}
+
+
+#' @rdname pval
+#' @method .pval summary.aov
+#' @export .pval.summary.aov
+#' @export
+.pval.summary.aov <- function(x) {
+  x |>
+    lapply(FUN = .pval.anova) |>
+    unlist()
+}
+
